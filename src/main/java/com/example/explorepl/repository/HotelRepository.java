e007d33ef9e9fd0f453670f08940b348a4defe5f
tblazejowski <tomasz.blazejowski@gmail.com>
2018-09-01 20:13:36 +0200

    Access to modify data limited.

--- src/main/java/com/example/explorepl/repository/HotelRepository.java
+++ src/main/java/com/example/explorepl/repository/HotelRepository.java
@@ -5,9 +5,34 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 public interface HotelRepository extends PagingAndSortingRepository<Hotel, Integer> {
     Page<Hotel> findByProvinceCode (@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Hotel> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Hotel> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Hotel hotel);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Hotel> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/example/explorepl/repository/ProvinceRepository.java
+++ src/main/java/com/example/explorepl/repository/ProvinceRepository.java
@@ -3,9 +3,34 @@ package com.example.explorepl.repository;
 import com.example.explorepl.domain.Province;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
 public interface ProvinceRepository extends CrudRepository<Province, String> {
     Optional<Province> findByName(@Param("name") String name);
+
+    @Override
+    @RestResource (exported = false)
+    <S extends Province> S save(S s);
+
+    @Override
+    @RestResource (exported = false)
+    <S extends Province> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource (exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource (exported = false)
+    void delete(Province province);
+
+    @Override
+    @RestResource (exported = false)
+    void deleteAll(Iterable<? extends Province> iterable);
+
+    @Override
+    @RestResource (exported = false)
+    void deleteAll();
 }
