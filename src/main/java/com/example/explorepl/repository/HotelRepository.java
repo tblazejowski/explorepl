3217e6bc4d2e588e71ee92fbb07e091db00d983d
tblazejowski <tomasz.blazejowski@gmail.com>
2018-09-01 20:01:26 +0200

    HotelRepository changed to extend PagingAndSortingRepository

--- src/main/java/com/example/explorepl/repository/HotelRepository.java
+++ src/main/java/com/example/explorepl/repository/HotelRepository.java
@@ -1,11 +1,13 @@
 package com.example.explorepl.repository;
 
 import com.example.explorepl.domain.Hotel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface HotelRepository extends CrudRepository <Hotel, Integer> {
-    List<Hotel> findByProvinceCode (@Param("code") String code);
+public interface HotelRepository extends PagingAndSortingRepository<Hotel, Integer> {
+    Page<Hotel> findByProvinceCode (@Param("code") String code, Pageable pageable);
 }
